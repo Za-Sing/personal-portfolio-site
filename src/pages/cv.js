2cd31965ae1d9962623fb86cb41c5c35660944e6
Za-Sing <za.sing227@gmail.com>
2021-08-24 22:53:31 -0500

    Added prelim table for CV

--- src/pages/cv.js
+++ src/pages/cv.js
@@ -1,48 +1,44 @@
 import * as React from "react";
-import { Link } from "gatsby";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
+import { profExperience } from "../components/layout.module.css"
 
 const CVPage = () => (
 	<Layout>
 		<Seo title="My CV" />
 		<h1>Professional Experience</h1>
-        <br></br>
-		<div style={{margin: 'auto', display: 'flex', width: '45rem'}}>
-			<div style={{flex: '50%', textAlign: 'left'}}>
-                Quality Assurance Analyst <br></br>
-				<a href="https://www.nextep.com/">Nextep Inc</a> <br></br>
-				March 2021 — Present
-			</div>
-			<div style={{flex: '50%', textAlign: 'right'}}>
-				I do stuff and things at Nextep
-			</div>
-		</div>
-		{/*<table style={{margin: 'auto', width: '50rem'}}>
+		<table className={ profExperience }>
 			<tr>
 				<td>
-					Quality Assurance Analyst <br></br>
-					<a href="https://www.nextep.com/">Nextep Inc</a> <br></br>
+					Quality Assurance Analyst<br></br>
+					<a href="https://www.nextep.com/">Nextep Inc</a><br></br>
 					March 2021 — Present
 				</td>
-                <td>
+				<td>
                     I do stuff and things at Nextep
                 </td>
 			</tr>
-        </table>*/}
-		{/*<div style={{margin: 'auto', textAlign: 'left', width: '50rem'}}>
-            Quality Assurance Analyst <br></br>
-            <a href="https://www.nextep.com/">Nextep Inc</a> <br></br>
-            March 2021 — Present 
-            <br></br><br></br><br></br><br></br>
-            Computer Lab Assistant <br></br>
-            University of Oklahoma — JRCoE <br></br>
-            August 2019 — May 2021
-            <br></br><br></br><br></br><br></br>
-            Sales Associate <br></br>
-            Walmart Inc<br></br>
-            May 2019 — August 2019, May 2020 — August 2020 
-        </div>*/}
+			<tr>
+				<td>
+					Computer Lab Assistant<br></br>
+					University of Oklahoma<br></br>
+					August 2019 — May 2021
+				</td>
+				<td>
+                    I did stuff and things at OU
+                </td>
+			</tr>
+            <tr>
+				<td>
+                    Sales Associate<br></br>
+                    Walmart Inc<br></br>
+                    May 2019 — August 2019, May 2020 — August 2020
+				</td>
+				<td>
+                    I did stuff and things at Walmart
+                </td>
+			</tr>
+		</table>
 	</Layout>
 );
 
